9af1d4cf8920abad684525097a5ffa827512cdb2
sudeep7regmi <sudeepregmi343@gmail.com>
2026-08-14 09:05:08 +0545

    change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,5 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Sudeep Regmi-cv",
-  description: "This sudeep Regmi`s resume",
-};
+import InteractiveBackground from "@/components/InteractiveBackground";
 
 export default function RootLayout({
   children,
@@ -23,12 +7,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body className="bg-white text-gray-950">
+        <InteractiveBackground />
+        {children}
+      </body>
     </html>
   );
 }
+
